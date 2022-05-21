7a92f547d344c54748fe1c56e19598c212ce4b35
lauren-acrich <94992940+lauren-acrich@users.noreply.github.com>
2022-05-21 14:31:11 -0700

    integrated chrome extension with Dev Tools

---
+++ background.js
@@ -0,0 +1,13 @@
+// let color = '#3aa757';
+
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.storage.sync.set({ color });
+//     console.log('Default background color set to %cgreen', `color: ${color}`);
+//   });
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
