3f816d67ecb55bb6cdb48b5c800fa45f8be8f343
mfiloramo <mlfiloramo@gmail.com>
2022-07-08 05:01:11 -0400

    added basic backend script -- still does not work on frontend yet

---
+++ contentscript.js
@@ -0,0 +1,5 @@
+const th = document.getElementsByTagName('body')[0];
+const s = document.createElement('script');
+s.setAttribute('type', 'text/javascript');
+s.setAttribute('src', 'chrome-extension://ndmgkbgkgpablbajfjdlhalfbnmcbpml/script.js');
+th.appendChild(s);
