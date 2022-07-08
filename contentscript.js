d1597d2da336884a8f6038e1097a2e4cacbd44a9
mfiloramo <mlfiloramo@gmail.com>
2022-07-08 10:58:43 -0400

    fixed contentscript src pointer

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,5 @@
 const th = document.getElementsByTagName('body')[0];
 const s = document.createElement('script');
 s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'chrome-extension://ndmgkbgkgpablbajfjdlhalfbnmcbpml/script.js');
+s.setAttribute('src', 'chrome-extension://gbaoapbpjbfkjacnebobjbpmnnidhjno/script.js');
 th.appendChild(s);
