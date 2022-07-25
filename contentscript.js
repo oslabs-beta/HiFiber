49e745bea7ed22dba9eabf084705994030918401
mfiloramo <mlfiloramo@gmail.com>
2022-07-25 09:21:16 -0400

    used various approaches to log the document across scripts

--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,22 @@
-// const th = document.getElementsByTagName('body')[0];
-// const s = document.createElement('script');
-// s.setAttribute('type', 'text/javascript');
-// s.setAttribute('src', 'chrome-extension://gbaoapbpjbfkjacnebobjbpmnnidhjno/script.js');
-// th.appendChild(s);
+import {rootNode} from "./script";
+
+// TRYING TO LOG THE ROOT NODE FROM THE CONTENTSCRIPT.JS BUT CAN'T YET
+console.log('This is the root node from the contentscript.js');
+console.log(rootNode);
+
+// CAN WE IMPORT THE ROOT NODE FROM SCRIPT.JS AND EXPORT IT, OR MOUNT IT, TO THE DOM? TO THE APP?
+
+// const mainPage = document.getElementsByTagName('body')[0];
+//
+// // WHAT IF THIS WAS THE BUNDLE.JS?
+// const mountedScript = document.createElement('script');
+// mountedScript.setAttribute('type', 'module');
+// mountedScript.setAttribute('src', 'chrome-extension://gbaoapbpjbfkjacnebobjbpmnnidhjno/script.js');
+// mountedScript.setAttribute('src', 'chrome-extension://gbaoapbpjbfkjacnebobjbpmnnidhjno/build/bundle.js');
+// mainPage.appendChild(mountedScript);
 
 // if you want to manipulate the DOM, you need the content script.
 // this script runs in the browser - on all URLs in our case
+// console.log('This is the contentscript. The window document object, ran at document_idle, should be below...');
+// console.dir(Object.values(window.document.body));
 
-
-console.dir(Object.values(window.document.body));
-// console.log(curren
-
-console.log("")
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /** The 'alternate' of the current fiber is the fiber in progress, and the 'alternate' of the fiber in progress is the current fiber. The current fiber represents what is rendered already, and the in-progress fiber is conceptually the stack frame that has not returned. * */
 
 /** The main disadvantage of console.dir is that console.dir accepts only one object as an argument, but console.log can log multiple arguments at once * */
-// console.log('debug');
-const current = Object.values(window.document.body.children[0])[0].alternate;
-// let current = Object.values(window.document.body.children[0])[0].alternate;
-console.dir(window.document.body);
-console.log(current);
 
-// while (current.child) {
-//   console.log(current);
-//   current = current.child;
-// }
+// DEFINE THE ROOT NODE, WHICH POINTS TO THE VALUES OF THE FIRST CHILD IN THE DIFFED DOM
+const rootNode = Object.values(window.document.body.children[0])[0];
 
-console.log(current);
+// CONSOLE.LOG THE ROOT FIBER NODE FROM THE CONSOLE OF THE APP VIA SCRIPT.JS
+// THIS SCRIPT ALSO SEEMS TO RUN TWICE IN THE CONSOLE EACH INITIALIZATION OF THE APP AND EXTENSION -- WHY?
+// FIRST IT LOGS AS undefined, THEN THE SCRIPT FROM APP.JSX RUNS, THEN THIS SCRIPT RUNS AGAIN WITH THE ROOT NODE.
+console.log('This is the root node from script.js');
+console.log(rootNode);
+
+// EXPORT THE ROOT NODE FOR OTHER PARTS OF THE APP TO ACCESS IT
+export { rootNode }
